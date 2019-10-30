e6acdfa0726ee0ae0434a641763f86a878c5c5fe
LongPhung93 <longhaiphung@gmail.com>
2019-10-30 17:49:16 +0700

    My first commit

---
+++ lythuyet.php
@@ -0,0 +1,23 @@
+<?php
+//Swich - case
+$data='bye';
+switch($data){
+    case 10: echo 'day la so 10';break;
+    case 'ok': echo 'day la ok';break;
+    case 'xin chao': echo 'day la xin chao';break;
+    case 'hello': echo 'day la so hello';break;
+    default: echo 'toi thuc thi nhe';
+}
+//include-require: goi 1 file PHP ben ngoai vao de thuc thi
+//incluce();
+//include_once();
+//require();
+//require_once();
+
+
+//Cai dat Github
+//git config --global user.name "LongPhung93"
+//git config --global user.email longhaiphung@gmail.com
+//git config --list
+
+?>
